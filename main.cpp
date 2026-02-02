190415f316d0402344ca8e55fcfe28e388f846ea
Jai <jaibanerjea@gmail.com>
2026-02-01 23:55:33 -0800

    map movement vector

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main(){
     const int window_width{384};
@@ -7,13 +8,26 @@ int main(){
     InitWindow(window_width, window_height, "My RPG");
 
     Texture2D game_map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2 map_pos{0.0, 0.0};
+
+    float speed{4.0};
+
 
     SetTargetFPS(60);
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(WHITE);
 
-        Vector2 map_pos{0.0, 0.0};
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+
+        if (Vector2Length(direction) != 0.0){
+            map_pos = Vector2Subtract(map_pos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+
         DrawTextureEx(game_map, map_pos, 0.0, 4.0, WHITE);
 
         EndDrawing();
